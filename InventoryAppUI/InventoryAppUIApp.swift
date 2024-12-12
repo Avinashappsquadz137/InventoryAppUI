9c4053d24755b642096e1bd817a0adb163e09cd0
Avinash Gupta <>
2024-12-12 11:49:07 +0530

    Add Fist Commit

---
+++ InventoryAppUI/InventoryAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryAppUIApp.swift
+//  InventoryAppUI
+//
+//  Created by Sanskar IOS Dev on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
